15be705099990ce2e232bfbebb87b4107db88ff3
zrone <xujining2008@126.com>
2026-02-13 23:41:18 +0800

    0213 update RecordSize => 28

--- go.mod
+++ go.mod
@@ -1,9 +1,17 @@
 module github.com/JMVoid/ipip2mmdb
 
-go 1.14
+go 1.24.0
 
 require (
-	github.com/maxmind/mmdbwriter v0.0.0-20200911190049-91ab57d2e8e9
+	github.com/maxmind/mmdbwriter v1.2.0
 	github.com/oschwald/geoip2-golang v1.4.0
 	github.com/sirupsen/logrus v1.6.0
+)
+
+require (
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	github.com/oschwald/maxminddb-golang v1.7.1-0.20200819192241-1f1e288ee3f9 // indirect
+	github.com/oschwald/maxminddb-golang/v2 v2.1.1 // indirect
+	go4.org/netipx v0.0.0-20231129151722-fdeea329fbba // indirect
+	golang.org/x/sys v0.38.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,66 +3,97 @@ package main
 import (
 	"bufio"
 	"flag"
+	"os"
+
 	"github.com/maxmind/mmdbwriter"
 	"github.com/maxmind/mmdbwriter/mmdbtype"
 	log "github.com/sirupsen/logrus"
-	"os"
 )
-		//"country": mmdbtype.Map{
-		//"continent": mmdbtype.Map{
-		//"code": mmdbtype.String("AS"),
-		//"geoname_id": mmdbtype.Uint32(6255147),
-		//"names":mmdbtype.Map{"de":mmdbtype.String("Asien"),
-		//"en":mmdbtype.String("Asia"),
-		//"es":mmdbtype.String("Asia"),
-		//"fr":mmdbtype.String("Asie"),
-		//"ja":mmdbtype.String("アジア"),
-		//"pt-BR":mmdbtype.String("Ásia"),
-		//"ru":mmdbtype.String("Азия"),
-		//"zh-CN":mmdbtype.String("亚洲")},
-		//},
-		//"country": mmdbtype.Map{
-		//"geoname_id":mmdbtype.Uint32(1814991),
-		//"is_in_european_union":mmdbtype.Bool(false),
-		//"iso_code":mmdbtype.String("CN"),
-		//"names":mmdbtype.Map{
-		//"de":mmdbtype.String("China"),
-		//"en":mmdbtype.String("China"),
-		//"es":mmdbtype.String("China"),
-		//"fr":mmdbtype.String("Chine"),
-		//"ja":mmdbtype.String("中国"),
-		//"pt-BR":mmdbtype.String("China"),
-		//"ru":mmdbtype.String("Китай"),
-		//"zh-CN":mmdbtype.String("中国"),
-		//},
-		//},
-		//"registered_country": mmdbtype.Map{
-		//"geoname_id":mmdbtype.Uint32(1814991),
-		//"is_in_european_union":mmdbtype.Bool(false),
-		//"iso_code":mmdbtype.String("CN"),
-		//"names":mmdbtype.Map{
-		//"de":mmdbtype.String("China"),
-		//"en":mmdbtype.String("China"),
-		//"es":mmdbtype.String("China"),
-		//"fr":mmdbtype.String("Chine"),
-		//"ja":mmdbtype.String("中国"),
-		//"pt-BR":mmdbtype.String("China"),
-		//"ru":mmdbtype.String("Китай"),
-		//"zh-CN":mmdbtype.String("中国"),
-		//},
-		//},
-		//"traits": mmdbtype.Map{
-		//"is_anonymous_proxy": mmdbtype.Bool(false),
-		//"is_satellite_provider":mmdbtype.Bool(false),
-		//},
-		//},
+
+//"country": mmdbtype.Map{
+//"continent": mmdbtype.Map{
+//"code": mmdbtype.String("AS"),
+//"geoname_id": mmdbtype.Uint32(6255147),
+//"names":mmdbtype.Map{"de":mmdbtype.String("Asien"),
+//"en":mmdbtype.String("Asia"),
+//"es":mmdbtype.String("Asia"),
+//"fr":mmdbtype.String("Asie"),
+//"ja":mmdbtype.String("アジア"),
+//"pt-BR":mmdbtype.String("Ásia"),
+//"ru":mmdbtype.String("Азия"),
+//"zh-CN":mmdbtype.String("亚洲")},
+//},
+//"country": mmdbtype.Map{
+//"geoname_id":mmdbtype.Uint32(1814991),
+//"is_in_european_union":mmdbtype.Bool(false),
+//"iso_code":mmdbtype.String("CN"),
+//"names":mmdbtype.Map{
+//"de":mmdbtype.String("China"),
+//"en":mmdbtype.String("China"),
+//"es":mmdbtype.String("China"),
+//"fr":mmdbtype.String("Chine"),
+//"ja":mmdbtype.String("中国"),
+//"pt-BR":mmdbtype.String("China"),
+//"ru":mmdbtype.String("Китай"),
+//"zh-CN":mmdbtype.String("中国"),
+//},
+//},
+//"registered_country": mmdbtype.Map{
+//"geoname_id":mmdbtype.Uint32(1814991),
+//"is_in_european_union":mmdbtype.Bool(false),
+//"iso_code":mmdbtype.String("CN"),
+//"names":mmdbtype.Map{
+//"de":mmdbtype.String("China"),
+//"en":mmdbtype.String("China"),
+//"es":mmdbtype.String("China"),
+//"fr":mmdbtype.String("Chine"),
+//"ja":mmdbtype.String("中国"),
+//"pt-BR":mmdbtype.String("China"),
+//"ru":mmdbtype.String("Китай"),
+//"zh-CN":mmdbtype.String("中国"),
+//},
+//},
+//"traits": mmdbtype.Map{
+//"is_anonymous_proxy": mmdbtype.Bool(false),
+//"is_satellite_provider":mmdbtype.Bool(false),
+//},
+//},
 
 var (
-	srcFile string
-	dstFile string
+	srcFile      string
+	dstFile      string
 	databaseType string
-	cnRecord = mmdbtype.Map{
+	cnRecord     = mmdbtype.Map{
+		"continent": mmdbtype.Map{
+			"code":       mmdbtype.String("AS"),
+			"geoname_id": mmdbtype.Uint32(6255147),
+			"names": mmdbtype.Map{
+				"de":    mmdbtype.String("Asien"),
+				"en":    mmdbtype.String("Asia"),
+				"es":    mmdbtype.String("Asia"),
+				"fr":    mmdbtype.String("Asie"),
+				"ja":    mmdbtype.String("アジア"),
+				"pt-BR": mmdbtype.String("Ásia"),
+				"ru":    mmdbtype.String("Азия"),
+				"zh-CN": mmdbtype.String("亚洲"),
+			},
+		},
 		"country": mmdbtype.Map{
+			"geoname_id":           mmdbtype.Uint32(1814991),
+			"is_in_european_union": mmdbtype.Bool(false),
+			"iso_code":             mmdbtype.String("CN"),
+			"names": mmdbtype.Map{
+				"de":    mmdbtype.String("China"),
+				"en":    mmdbtype.String("China"),
+				"es":    mmdbtype.String("China"),
+				"fr":    mmdbtype.String("Chine"),
+				"ja":    mmdbtype.String("中国"),
+				"pt-BR": mmdbtype.String("China"),
+				"ru":    mmdbtype.String("Китай"),
+				"zh-CN": mmdbtype.String("中国"),
+			},
+		},
+		"registered_country": mmdbtype.Map{
 			"geoname_id":           mmdbtype.Uint32(1814991),
 			"is_in_european_union": mmdbtype.Bool(false),
 			"iso_code":             mmdbtype.String("CN"),
@@ -80,18 +111,19 @@ var (
 	}
 )
 
-func init()  {
+func init() {
 	flag.StringVar(&srcFile, "s", "ipip_cn.txt", "specify source ip list file")
 	flag.StringVar(&dstFile, "d", "Country.mmdb", "specify destination mmdb file")
-	flag.StringVar(&databaseType,"t", "GeoIP2-Country", "specify MaxMind database type")
+	flag.StringVar(&databaseType, "t", "GeoIP2-Country", "specify MaxMind database type")
 	flag.Parse()
 }
 
-func main()  {
+func main() {
 	writer, err := mmdbwriter.New(
 		mmdbwriter.Options{
 			DatabaseType: databaseType,
-			RecordSize:   24,
+			RecordSize:   28,
+			Languages:    []string{"de", "en", "es", "fr", "ja", "pt-BR", "ru", "zh-CN"},
 		},
 	)
 	if err != nil {
@@ -129,5 +161,3 @@ func main()  {
 	}
 
 }
-
-
